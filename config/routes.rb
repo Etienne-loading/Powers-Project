f81f79e1c020d0750588b2511d2eae7e7df714a3
antoine chilewski <chilewski.a@gmail.com>
2023-05-30 12:01:09 +0200

    create routes/controllers/views for powers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "powers#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  resources :powers, except: [:destroy, :update, :edit] do
+    resources :bookings, only: [:new, :create]
+  end
 end
