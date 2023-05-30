6c1b0ad8b7c495a1f5e4824371a3defdb30a35f8
Jean-Baptiste Bernard <jb.bernard92@gmail.com>
2023-05-30 13:09:19 +0200

    updating new view and validations

--- app/models/power.rb
+++ app/models/power.rb
@@ -4,11 +4,12 @@ class Power < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
 
+  CATEGORY = ["Hogwart", "The Legend of Zelda", "Flash", "Star Wars", "Dragon Ball"]
+  RARITY = ["Legendary", "Epic", "Rare", "Uncommon"]
   validates :name, presence: true
   validates :price, presence: true
   validates :description, presence: true
-  validates :rarity, presence: true
-  validates :category, presence: true
+  validates :rarity, presence: true, inclusion: { in: RARITY }
+  validates :category, presence: true, inclusion: { in: CATEGORY }
   validates :universe, presence: true
-
 end
