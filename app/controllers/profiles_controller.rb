ff1dc4ef1e3fd7a3d1d2e59510dc9664dcb30a8e
Jean-Baptiste Bernard <jb.bernard92@gmail.com>
2023-05-30 17:22:05 +0200

    Dashboard view and dashboard wontroller

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,16 @@
+class ProfilesController < ApplicationController
+  before_action :find_profile, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  private
+
+  def find_profile
+    @profile = current_user
+  end
+
+  def profile_params
+    params.permit(:user).permit(:pseudo, :address, :first_name, :last_name, :power_id)
+  end
+end
