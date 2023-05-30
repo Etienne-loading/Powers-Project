615703a979c7a41b6bc66c48ee0cc59375d1d747
Clément Ritz <clement.ritz@orange.fr>
2023-05-30 17:55:25 +0200

    Form bookings

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -7,6 +7,7 @@ class PowersController < ApplicationController
   end
 
   def show
+    @booking = Booking.new
   end
 
   def new
---
+++ app/javascript/controllers/datepicker_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
+
+// Connects to data-controller="datepicker"
+export default class extends Controller {
+  static targets = ['startDate', 'endDate', 'total', 'price']
+
+  connect() {
+    const date = new Date
+    flatpickr(this.startDateTarget, {
+      mode: "single",
+      minDate: Date.now(),
+      dateFormat: "Y-m-d",
+    });
+    flatpickr(this.endDateTarget, {
+      mode: "single",
+      minDate: date.setDate(date.getDate()+1),
+      dateFormat: "Y-m-d",
+    });
+  }
+  totalupdate() {
+    console.log(this.startDateTarget.value);
+    const diff = Date.parse(this.endDateTarget.value) - Date.parse(this.startDateTarget.value)
+    const days = Math.ceil(diff / (1000 * 3600 * 24))
+    const price = parseInt(this.priceTarget.innerText)
+
+    if (this.endDateTarget.value && this.startDateTarget.value) {
+      this.totalTarget.innerText = `Total: ${days * price} $`
+    } else {
+      this.totalTarget.innerText = `Total: 0 $`
+    }
+  }
+}
