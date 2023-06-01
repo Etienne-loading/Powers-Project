43b404f52cf2f0cfc4d55a7065fbd2ecf97fe609
Etienne Pagenaud <e.pagenaud@gmail.com>
2023-06-01 13:11:26 +0200

    nav bar sticky + home routes changed + content on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "powers#index"
+  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :profiles, only: [:show, :edit, :update] do
-  end
+  resources :profiles, only: [:show, :edit, :update]
 
   # Defines the root path route ("/")
   resources :powers do
